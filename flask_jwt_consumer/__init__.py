5bdcbb9f466abf66061de0a96fe2e3d916482f9d
Ian Taylor <ian.taylor@syapse.com>
2022-09-29 16:26:50 -0400

    try this

--- flask_jwt_consumer/__init__.py
+++ flask_jwt_consumer/__init__.py
@@ -1,5 +1,13 @@
 name = "flask_jwt_consumer"
 
+from pkg_resources import DistributionNotFound, get_distribution
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    __version__ = "0.0.0"
+
+
 from .flask_jwt_consumer import JWTConsumer
 from .helpers import get_jwt_payload, get_jwt_raw
 from .decorators import requires_jwt
